52b6441eb7e9c78fa5dde91adcf79a82cfee73e9
sahil <rohillasahil704@gmail.com>
2024-10-23 00:36:13 -0700

    updated

--- src/components/MyTeam/MyTeam.jsx
+++ src/components/MyTeam/MyTeam.jsx
@@ -10,14 +10,14 @@ const MyTeam = () => {
         "Edu":" A MERN Stack Developer specializes in building web applications using MongoDB, Express.js, React, and Node.js.They handle both frontend and backend development, create APIs."
     },
     {
-        "id": 1,
+        "id": 2,
        "image":"src/assets/woman.png",
         "name": "Nehe Verma",
         "Title":"Backend Devloper",
         "Edu":"  A Back-End Devloper focuses on server-side logic, databases,and API integration. They work with technologies like Node.js, databases, and server architecture to handle data storage, security."
     },
     {
-        "id": 1,
+        "id": 3,
        "image":"src/assets/boy.png",
         "name": "Aman Kumar",
         "Title":"FrountEnd Devloper ",
